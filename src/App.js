a009d3fd4730f775268230b39a6d56cb593bce70
Anand P <134688156+Anand3643@users.noreply.github.com>
2023-08-31 20:12:21 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import Navbar from "./components/Navbar/navbar";
+import Intro from "./components/Intro/Intro";
+import About from "./components/About/About";
+import Project from "./components/Projects/Project";
+import Certificate from "./components/Certificate/Certificate";
+import Bottem from "./components/Bottem/Bottem";
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar/>
+      <Intro/>
+      <About/>
+      <Project/>
+      <Certificate/>
+      <Bottem/>
+     
+     
+
     </div>
   );
 }
